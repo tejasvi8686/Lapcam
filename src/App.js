215c43e0f80f7bf53dc64f681b27198413a8bbdd
tejasvi8686 <tejasviraj8686@gmail.com>
2023-05-14 21:31:17 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,52 @@
-import logo from './logo.svg';
+// eslint-disable-next-line no-unused-vars
 import './App.css';
+import Item from './components/Item';
+import ItemDate from './components/ItemDate';
+import Card from './components/Card';
+
 
 function App() {
+
+  const response = [
+    {
+      itemName: "Nirma",
+      itemdate: "20",
+      itemMonth: "june",
+      itemYear: "1998",
+    },
+
+    {
+      itemName: "Nirma2",
+      itemdate: "202",
+      itemMonth: "june2",
+      itemYear: "19982",
+    },
+
+    {
+      itemName: "Nirma3",
+      itemdate: "203",
+      itemMonth: "june3",
+      itemYear: "19983",
+    }
+  ];
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div>
+
+    <Card><Item name={response[0].itemName}> Hello jee mai apki first iteam</Item>
+   <ItemDate  day={response[0].itemdate} month={response[0].itemMonth} year={response[0].itemYear}></ItemDate>
+
+   <Item name={response[1].itemName}></Item>
+   <ItemDate day={response[1].itemdate} month={response[1].itemMonth} year={response[1].itemYear}></ItemDate>
+
+   <Item  name={response[2].itemName} ></Item>
+   <ItemDate day={response[2].itemdate} month={response[2].itemMonth} year={response[2].itemYear}></ItemDate></Card>
+
+  
+   
+
+   <div className='App'>Hello jee</div>
+    
+   </div>
   );
 }
 
---
+++ src/components/Item.js
@@ -0,0 +1,17 @@
+import './Item.css'
+
+function Item (props) {
+
+    const itemName  = props.name;
+
+    return(
+        <div>
+            <p className='Nirma'>{itemName}</p>
+            {props.children}
+        </div>
+    );
+
+
+}
+
+export default Item;
